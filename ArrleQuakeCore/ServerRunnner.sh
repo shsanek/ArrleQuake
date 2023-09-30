0999eb8286b2c43191d00194ad26528d892057f3
Alexander Shipin <ssanek212@gmail.com>
2023-09-30 13:07:55 +0200

    fix base script

--- ArrleQuakeCore/ServerRunnner.sh
+++ ArrleQuakeCore/ServerRunnner.sh
@@ -3,8 +3,13 @@ if [ -d "ArrleQuake" ]; then
     git checkout
     git remote update
     git pull
-
 else
+    mkdir log
+    mkdir run
+    cd run
+    mkdir debug
+    mkdir release
+    cd ..
     git clone git@github.com:shsanek/ArrleQuake.git
     cd ArrleQuake
 fi
